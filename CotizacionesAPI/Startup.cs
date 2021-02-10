a1c4215fa10ee5f19ca1128537ff211d85538ac7
david <dscarso@advantive.com.ar>
2021-02-10 08:51:15 -0300

    add swagger

--- CotizacionesAPI/Startup.cs
+++ CotizacionesAPI/Startup.cs
@@ -24,6 +24,16 @@ namespace CotizacionesAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwaggerUI();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
